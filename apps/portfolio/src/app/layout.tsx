21460c2c85ef0b10996e7ecac8466f82a65daea0
Devvrat Shukla <devvratshukla@gmail.com>
2024-05-27 16:59:42 +0100

    refactor: moving fonts to separate file and adding antialiased

--- apps/portfolio/src/app/layout.tsx
+++ apps/portfolio/src/app/layout.tsx
@@ -1,28 +1,22 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 import Header from '@/components/header';
 import '@/styles/globals.css';
 import React from 'react';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
+import { inter } from '@/lib/font';
 
 export const metadata: Metadata = {
   title: 'Portfolio',
   description: 'Portfolio of Devvrat Shukla',
 };
 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-});
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en" className={`${inter.className} antialiased`}>
       <body className="bg-warm-black mx-auto flex min-h-screen w-full scroll-my-12 flex-col overflow-auto">
         <div className="medium-up large container sticky top-0 z-[100]">
           <Header>
