bc5a5812355bae3d678ccaeff7a558cd5ea0bffe
cypherkunp <cypherkunp@users.noreply.github.com>
2024-06-03 00:07:01 +0000

    deploy: 0f82cdceb1f848739d2730e87c98918aefe9fe9e

--- apps/portfolio/src/lib/env.utils.ts
+++
@@ -1,4 +0,0 @@
-export const NEXT_PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL;
-
-//SERVER SIDE ENV VARIABLES
-export const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
--- packages/ui/src/components/ui/section.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@ui/lib/utils';
-import React from 'react';
-
-export interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Section({ className, ...props }: SectionProps) {
-  return <section className={cn('flex min-h-0 flex-col gap-y-5', className)} {...props} />;
-}
